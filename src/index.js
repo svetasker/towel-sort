864f930ebb653e8f7b1250ac575e1442164c9b16
Aleksandr <104507490+svetasker@users.noreply.github.com>
2023-02-23 03:21:14 +0300

    update info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 module.exports = function towelSort (matrix) {
   if (!matrix) return new Array();
   return matrix.map((item, index) => {
+      //создаем новый массив, в котором каждый четный элемент записывается в обратном порядке
     return (index % 2) ? item.reverse() : item;
   }).flat();
 }
